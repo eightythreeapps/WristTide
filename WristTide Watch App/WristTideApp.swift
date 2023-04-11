49d375532db504cd794639694473d0c1a7c9b837
Ben Reed <ben.reed@systemc.com>
2023-04-11 09:51:07 +0100

    Initial Commit
    Initial project set-up

---
+++ WristTide Watch App/WristTideApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristTideApp.swift
+//  WristTide Watch App
+//
+//  Created by Ben Reed on 11/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WristTide_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
